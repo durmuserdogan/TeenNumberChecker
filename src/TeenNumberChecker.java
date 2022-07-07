7e945f15c50c065a457fcfffb6a3bcfc8258736a
durmuserdogan <drmserdogan@outlook.com>
2022-07-07 11:59:57 +0300

    kkkk

--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,6 +1,6 @@
 public class TeenNumberChecker {
     public static boolean hasTeen(int one, int two, int three){
-        if ((one <=19 || two<=19  || three<=19 )  && (one>=13 || two>=13 || three>=13)){
+        if (((one <=19)&&(one>=13))  || ((two<=19)&&(two>=13))  || ((three<=19)&&(three>=13)) ){
             return true;
         }else{
             return false;
