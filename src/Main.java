0f0fae0e1c836f581f98926cae7235a7fe3432e2
durmuserdogan <drmserdogan@outlook.com>
2022-07-07 11:55:14 +0300

    kkkk

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        boolean t=TeenNumberChecker.hasTeen(9,19,99);
+        System.out.println(t);
+    }
 }
--- src/TeenNumberChecker.java
+++ src/TeenNumberChecker.java
@@ -1,2 +1,16 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen(int one, int two, int three){
+        if ((one <=19 || two<=19  || three<=19 )  && (one>=13 || two>=13 || three>=13)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static boolean isTeen(int param){
+        if(param>=13 && param<=19){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
